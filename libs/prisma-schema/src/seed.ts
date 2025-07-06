f6c9a93ab7e6a0d83122f11098dbcaa87d20728b
Paul Kukowski <kukowskipaul@gmail.com>
2025-07-06 10:20:02 +0200

    Seed script deletes all cards now

--- libs/prisma-schema/src/seed.ts
+++ libs/prisma-schema/src/seed.ts
@@ -3,17 +3,7 @@ import { PrismaClient } from '../../prisma-client/src/lib/generated/client';
 const prisma = new PrismaClient();
 
 async function main() {
-  const card1 = await prisma.card.upsert({
-    where: {
-      name: 'Uncle Istvan',
-    },
-    update: {},
-    create: {
-      name: 'Uncle Istvan',
-    },
-  });
-
-  console.log({ card1 });
+  await prisma.card.deleteMany();
 }
 
 main()
