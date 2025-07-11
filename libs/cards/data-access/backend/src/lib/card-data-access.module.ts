26b14f965d2ddd295a7d638df5f387b979a613e0
Paul Kukowski <kukowskipaul@gmail.com>
2025-07-11 10:28:47 +0200

    bleh

--- libs/cards/data-access/backend/src/lib/card-data-access.module.ts
+++ libs/cards/data-access/backend/src/lib/card-data-access.module.ts
@@ -3,10 +3,9 @@ import { Module } from '@nestjs/common';
 import { PrismaClientModule } from '@library/prisma-client';
 import { CardsController } from './cards.controller';
 import { CardsService } from './cards.service';
-import { CacheModule } from '@library/core/cache';
 
 @Module({
-  imports: [PrismaClientModule, CacheModule],
+  imports: [PrismaClientModule],
   controllers: [CardsController],
   providers: [CardsService],
   exports: [CardsService],
