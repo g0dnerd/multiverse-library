e6912678d121880c108aeef73cbd7351cb29e29c
Paul Kukowski <kukowskipaul@gmail.com>
2025-07-11 11:05:45 +0200

    I trust

--- apps/library/src/app/app.module.ts
+++ apps/library/src/app/app.module.ts
@@ -1,8 +1,7 @@
 import { CacheModule } from '@nestjs/cache-manager';
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { createKeyv, Keyv } from '@keyv/redis';
-import { CacheableMemory } from 'cacheable';
+import { createKeyv } from '@keyv/redis';
 
 import { CardDataAccessModule } from '@library/cards/data-access';
 import { ScryfallModule } from '@library/scryfall/data-access';
@@ -20,17 +19,9 @@ import { AppController } from './app.controller';
       isGlobal: true,
       inject: [ConfigService],
       useFactory: async (configService: ConfigService) => {
-        const host = configService.get<string>('REDIS_HOST');
-        const port = parseInt(configService.get<string>('REDIS_PORT')!, 10);
-        const username = configService.get<string>('REDIS_USERNAME');
-        const password = configService.get<string>('REDIS_PASSWORD');
+        const redisUrl = configService.get<string>('REDIS_URL');
         return {
-          stores: [
-            new Keyv({
-              store: new CacheableMemory({ ttl: '24h' }),
-            }),
-            createKeyv(`redis://${username}:${password}@${host}:${port}`),
-          ],
+          stores: [createKeyv(redisUrl)],
         };
       },
     }),
