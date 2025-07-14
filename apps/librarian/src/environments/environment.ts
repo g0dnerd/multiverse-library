0f80522b658ff3ad4915ec3287f04ec3e9cab31d
Paul Kukowski <paul.kukowski@webid-solutions.de>
2025-07-14 09:21:06 +0200

    Fix api url

--- apps/librarian/src/environments/environment.ts
+++ apps/librarian/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/api',
+  // apiUrl: 'http://localhost:3000/api',
+  apiUrl: 'https://multiverse-library-production.up.railway.app/api',
 };
